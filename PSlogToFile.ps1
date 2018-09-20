94da6bd2b0df96defc5ea52e47d34f894b4c15e1
Roy Tokeshi <roy.tokeshi@citrix.com>
2018-09-20 12:08:31 -0700

    Update PSlogToFile.ps1
    Change method to Start-Transcript

--- PSlogToFile.ps1
+++ PSlogToFile.ps1
@@ -1,18 +1,6 @@
-Function Main {
-    Log -File "D:\Apps\Logs\$Env:computername.log"
+# Verify AppData\Powershell-logs exists if not create it
+$pslogpath = "$env:appdata\PowerShell-Logs"
+if (!(test-path -path $pslogpath)) {new-item -path $pslogpath -itemtype directory}
 
-    $tcp = (get-childitem c:\windows\system32\drivers\tcpip.sys).Versioninfo.ProductVersionRaw
-    $dfs = (get-childitem C:\Windows\Microsoft.NET\Framework\v2.0.50727\dfsvc.exe).Versioninfo.ProductVersionRaw
-
-    Log "TCPIP.sys Version on $computer is:" $tcp
-    Log "DFSVC.exe Version on $computer is:" $dfs
-
-    If (get-wmiobject win32_share | where-object {$_.Name -eq "REMINST"}) {Log "The REMINST share exists on $computer"}
-    Else {Log "The REMINST share DOES NOT exist on $computer - Please create as per standards"}
-
-    "KB2450944", "KB3150513", "KB3176935" | ForEach {
-        $hotfix = Get-HotFix -Id $_ -ErrorAction SilentlyContinue
-        If ($hotfix) {Log -Color Green Hotfix $_ is installed}
-        Else {Log -Color Red Hotfix $_ " is NOT installed - Please ensure you install this hotfix"}
-    }
-}
+# Start logging into this directory with computername and date time stamp in filename
+Start-Transcript -Path "$env:appdata\PowerShell-Logs\$Env:computername-$(((get-date).ToUniversalTime()).ToString("yyyyMMddThhmmssZ")).log" -NoClobber
